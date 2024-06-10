d344630954cfaa8b767973fcc7e55064c2b05265
Anerys Menal <anerysmenal@gmail.com>
2024-06-10 18:50:56 -0500

    Initial commit

---
+++ Menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+#control with mouse 
+func _ready():
+	$VBoxContainer/PLayer.grab_focus()
+	
+func _on_p_layer_pressed():
+	get_tree().change_scene_to_file("res://first_level.tscn")
+
+
+func _on_option_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+	
